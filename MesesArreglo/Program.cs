e6e0f8b62517e7c3615fdbf5aae80990ae1cd0e0
Camila Fimbres <camifimbres@gmail.com>
2024-10-31 17:49:02 -0700

    Agregar archivos de proyecto.

---
+++ MesesArreglo/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MesesArreglo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce un número :3");
+            string[] mesecito = new string[12];
+
+            mesecito[0] = "Enero";
+            mesecito[1] = "Febrero";
+            mesecito[2] = "Marzo";
+            mesecito[3] = "Abril";
+            mesecito[4] = "Mayo";
+            mesecito[5] = "Junio";
+            mesecito[6] = "Julio";
+            mesecito[7] = "Agosto";
+            mesecito[8] = "Septiembre";
+            mesecito[9] = "Octubre";
+            mesecito[10] = "Noviembre";
+            mesecito[11] = "Diciembre";
+
+            string input = Console.ReadLine();
+            int.TryParse(input, out int result);
+
+            if (result > 0 && result <= 12)
+            {
+                Console.WriteLine(mesecito[result -1]);
+            }
+            else 
+            {
+                Console.WriteLine("EH! Esta mal :c");
+            }
+        }
+
+    }
+}
